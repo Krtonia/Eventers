3b030f73e519d7950250ee36029050edd9a4b427
Shikhar <Krtonia@gmail.com>
2024-10-11 14:00:07 +0530

    Finalize the Login page
    * Import and use FiraSans font
    * Import some icons from flaticons
    * Use rounded corners for some UI elements
    * Use "forgotPassword" button instead of text

--- app/src/main/java/com/example/eventer/ui/Start.kt
+++ app/src/main/java/com/example/eventer/ui/Start.kt
@@ -1,15 +1,32 @@
 package com.example.eventer.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +35,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -26,6 +46,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.eventer.R
+import com.example.eventer.ui.theme.firasans
 
 fun <T> List(size: Unit, init: Unit) {
 
@@ -33,40 +55,129 @@ fun <T> List(size: Unit, init: Unit) {
 
 @Composable
 fun Start() {
-    val Colors = listOf(
-        Color.Black,
-        Color.Blue,
-        Color.Magenta)
+    val gc = listOf(
+        Color(0xFF353E43),
+        Color(0xFF252525),
+        Color(0xFF6A5ACD))
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(
                 brush = Brush.linearGradient(
-                    colors = Colors,
+                    colors = gc
                 )
             ),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
+
+        Image(modifier = Modifier.size(350.dp),
+            painter = painterResource(id = R.drawable.model_fg),
+            contentDescription ="Login image" )
+
         Text(
             modifier = Modifier,
-            text = "Welcome to Eventers\n \n ",
+            text = "Welcome to Eventers",
+            fontFamily = firasans,
             color = Color.White,
             fontStyle = FontStyle.Italic,
             fontSize = 38.sp,
             fontWeight = FontWeight.ExtraBold,
             textAlign = TextAlign.Right
         )
-        val name = remember { mutableStateOf(TextFieldValue(""))}
-        TextField(
-            value = name.value,
-            onValueChange = { name.value = it },
-            placeholder = { Text(text = "Login-ID", lineHeight = 15.sp)},
+
+        Text(text = "Login to your Account",
+            color = Color.White,
+            fontFamily = firasans,
+            fontWeight = FontWeight.SemiBold
         )
 
+        Spacer(modifier = Modifier.height(25.dp))
+
+        val name = remember { mutableStateOf(TextFieldValue(""))}
+        OutlinedTextField(
+            value = name.value,
+            shape = RoundedCornerShape(25.dp),
+            textStyle = TextStyle(color = Color.White,),
+            onValueChange = { name.value = it },
+            placeholder = { Text(text = "Email", lineHeight = 15.sp, color = Color.White,) },
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
         val password = remember { mutableStateOf(TextFieldValue(""))}
-        TextField(value = password.value,
+        OutlinedTextField(
+            value = password.value,
+            shape = RoundedCornerShape(25.dp),
+            textStyle = TextStyle(color = Color.White,),
             onValueChange ={ password.value = it},
-            placeholder = { Text(text = "Password",lineHeight = 15.sp)})
+            placeholder = { Text(text = "Password",lineHeight = 15.sp, color = Color.White)})
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(modifier= Modifier
+            .width(180.dp)
+            .height(50.dp),onClick = {}) {
+            Text(text = "Login",
+                fontFamily = firasans,
+                color = Color.Black,
+                fontWeight = FontWeight.Medium,
+                fontSize = 18.sp)
+        }
+        Spacer(modifier = Modifier.height(18.dp))
+
+        Button(onClick = {  }, modifier = Modifier.height(50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.White)) {
+            Text(text = "Forgot password",
+                fontFamily = firasans,
+                fontWeight = FontWeight.Medium,
+                fontSize = 17.sp,)
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "OR",
+            fontFamily = firasans,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 17.sp,)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Signup With",
+            color = Color.Black,
+            fontFamily = firasans,
+            fontWeight = FontWeight.ExtraBold,
+        fontSize = 20.sp)
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Row(modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Image(
+                modifier = Modifier.size(50.dp).clickable {  },
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = "Google"
+            )
+
+            Image(
+                modifier = Modifier.size(50.dp).clickable {  },
+                painter = painterResource(id = R.drawable.facebook),
+                contentDescription = "Facebook"
+            )
+
+            Image(
+                modifier = Modifier.size(50.dp).clickable {  },
+                painter = painterResource(id = R.drawable.twitter),
+                contentDescription = "Twitter"
+            )
+
+            Image(
+                modifier = Modifier.size(50.dp).clickable {  },
+                painter = painterResource(id = R.drawable.email),
+                contentDescription = "Email"
+            )
+        }
+
     }
 }
--- app/src/main/java/com/example/eventer/ui/theme/Type.kt
+++ app/src/main/java/com/example/eventer/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.eventer.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.eventer.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +34,23 @@ val Typography = Typography(
     )
     */
 )
+
+val firasans = FontFamily(
+    Font(R.font.firasans_black, FontWeight.Black),
+    Font(R.font.firasans_blackitalic, FontWeight.Normal),
+    Font(R.font.firasans_bold, FontWeight.Bold),
+    Font(R.font.firasans_bolditalic, FontWeight.Bold),
+    Font(R.font.firasans_extrabold, FontWeight.ExtraBold),
+    Font(R.font.firasans_extrabolditalic, FontWeight.ExtraBold),
+    Font(R.font.firasans_italic, FontWeight.Normal),
+    Font(R.font.firasans_light, FontWeight.Light),
+    Font(R.font.firasans_lightitalic, FontWeight.Light),
+    Font(R.font.firasans_medium, FontWeight.Medium),
+    Font(R.font.firasans_mediumitalic, FontWeight.Medium),
+    Font(R.font.firasans_regular, FontWeight.Normal),
+    Font(R.font.firasans_semibold, FontWeight.SemiBold),
+    Font(R.font.firasans_semibolditalic, FontWeight.SemiBold),
+    Font(R.font.firasans_thin, FontWeight.Thin),
+    Font(R.font.firasans_thinitalic, FontWeight.Thin),
+)
+
